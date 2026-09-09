2e6721205fd9095a8fcaf08fbb51e16e7540a2fd
netrumcodes <eemil.helin@gmail.com>
2026-09-09 16:50:02 +0300

    Tehtävät 1-6 tehty, päivitetty GITiin

---
+++ mod02/tehtävä 4.py
@@ -0,0 +1,8 @@
+import math
+kokonaisluku1 = int(input("Anna ensimmäinen kokonaisluku: "))
+kokonaisluku2 = int(input("Anna toinen kokonaisluku: "))
+kokonaisluku3 = int(input("Anna kolmas kokonaisluku: "))
+summa = kokonaisluku1 + kokonaisluku2 + kokonaisluku3
+tulo = kokonaisluku1 * kokonaisluku2 * kokonaisluku3
+keskimaarvo = summa / 3
+print(f"Kolmen kokonaisluvun summa on {summa}, tulo on {tulo}, ja keskiarvo on {keskimaarvo:.2f}")
---
+++ mod02/tehtävä 6.py
@@ -0,0 +1,6 @@
+import math
+import random
+kolmenumeroinen_koodi = str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9))
+nelinumeroinen_koodi = str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9))
+print(kolmenumeroinen_koodi)
+print(nelinumeroinen_koodi)
